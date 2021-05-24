c26977c768db1ca8a6a5f78a61db5bbe8c6b2581
A7mod <aamodkrishna@gmail.com>
2021-05-24 12:27:19 +0530

    added postgres client setup

--- server/index.js
+++ server/index.js
@@ -9,4 +9,15 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Postgres Client Setup
+
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
 
